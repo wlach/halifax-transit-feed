300386c1e48db87bd18036d9d6bb822938dad412
William Lachance <wrlach@gmail.com>
2008-08-26 13:02:30 -0300

    Don't put a comma on the last line when parsing times.

--- parse-times.pl
+++ parse-times.pl
@@ -2,44 +2,14 @@
 
 use strict;
 
-sub parse_time {
-    my ($time) = @_;
-    if ($time =~ /a\Z/) {
-	$time =~ s/([0-9]+)([0-9][0-9])a/\1:\2:00/;
-	if ($time =~ /^[0-9]:/) {
-	    $time = "0" . $time;
-	}
-    } elsif ($time =~ /p\Z/) {
-	my $hour = $time;
-	$hour =~ s/^([0-9]+)[0-9][0-9]p/\1/;
-	$time =~ s/[0-9]+([0-9][0-9])p/:\1:00/;
-	if ($hour < 12) {
-	    $hour += 12;
-	}
-	$time = $hour . $time;
-    } elsif ($time =~ /x\Z/) {
-	my $hour = $time;
-	$hour =~ s/^([0-9]+)[0-9][0-9]x/\1/;
-	$time =~ s/[0-9]+([0-9][0-9])x/:\1:00/;
-	if ($hour == 12) {
-	    $hour += 12;
-	} else {
-	    $hour += 24;
-	}
-	$time = $hour . $time;
-    } elsif ($time =~ /^-\Z/) {
-	# no stop at this time
-    } else {
-	print "Should not happen! Time misformed.\n";
-	exit;
-    }
-
-    return $time;
-
-}
-
+my $first = 1;
 while (<STDIN>) {
     if ($_ !~ /^\#/) {
+	if (!$first) {
+	    print ",\n";
+	} else {
+	    $first = 0;
+	}
 	chomp;
 	my @times = split /\ +/;
 	print "  [ ";
@@ -52,9 +22,9 @@ while (<STDIN>) {
 	    }
 	    print $_;
 	}
-	print "],\n";
+	print "]";
     } else {
 	print $_;
     }
-}  
+}
 
